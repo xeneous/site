e5c512bfad049fadeb6961c48187afd7a758442e
Daniel Ferreyra <daferreyra@roemmers.com.ar>
2024-10-17 11:56:47 -0300

    moving forward

--- main.py
+++ main.py
@@ -11,16 +11,26 @@ app = Flask(__name__, template_folder='templates')
 app.secret_key = "Cr0m0s0mat1c0"
 
 vPort = 5000
-
+#Configure SQL Alchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 # Database Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), unique=True, nullable=False)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash( self.password_hash, password )
 
 #ROutes
+#root
 @app.route('/')
 def home():
     if "username" in session:
@@ -28,11 +38,51 @@ def home():
     
     return render_template('index.html')
 
+#login
+@app.route('/login', methods=["POST"])
+def login():
+    #collect info from the from
+    postUsername=request.form['username']
+    postPassword=request.form['password']
+    user = User.query.filter_by(username=postUsername).first()
+
+    if user and user.chech_password(postPassword):
+        session['username'] = postUsername
+        return redirect(url_for,'Dashboard')
+    else:
+        return render_template("index.html")
+    
+    #Check if user exists
+
+    #if not return to home page
+
+#Register
+@app.route('/register', methods=["POST"])
+def register():
+    #collect info from the from
+    postUsername=request.form['username']
+    postPassword=request.form['password']
+    user = User.query.filter_by(username=postUsername).first()
+
+    if user:
+        return render_template("index.html",error = "User already exists.")
+    else:
+        new_User = User(username=postUsername)
+        new_User.set_password(postPassword)
+        db.session.add(new_User)
+        db.session.commit()
+        session['username'] = postUsername
+        return redirect(url_for,'Dashboard')
+    
+#Dashboard
+
+#logout
+
 mode = 'dev'
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all
+        db.create_all()
 
     if mode == 'dev':
         app.run(host='0.0.0.0', port=vPort, debug=True )
